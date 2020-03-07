e68bc152fe32ed7a86300a9adb980e72ebf0a25a
alyssalanae <briggs.l.alyssa@gmail.com>
2020-03-06 19:11:21 -0600

    added For and If statements

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 11, name: 'Hawkeye'},
+  {id: 12, name: 'Black Widow'},
+  {id: 13, name: 'Iron Man'},
+  {id: 14, name: 'Spiderman'},
+  {id: 15, name: 'Hulk'},
+  {id: 16, name: 'Dr Strange'},
+  {id: 17, name: 'Captain America'},
+  {id: 18, name: 'Thor'},
+  {id: 19, name: 'Black Panther'},
+  {id: 20, name: 'Deadpool'}
+];
